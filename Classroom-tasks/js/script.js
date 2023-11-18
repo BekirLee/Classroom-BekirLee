25a4726a053675e3f1e1060a8d1b097686ee60a8
BekirLee <nihadbekirli8@gmail.com>
2023-11-18 13:30:16 +0400

    add commit for every single codes about task

---
+++ Classroom-tasks/js/script.js
@@ -0,0 +1,242 @@
+let a = 54;
+let b = 23;
+let c = 3;
+
+//! Task 1
+
+// if (b % 2 == 0) {
+//     console.log("This number is Even")
+// }
+// else {
+//     console.log("This number is Odd")
+// }
+
+
+
+//! Task 2
+
+// if (a > b) {
+//     console.log(a + " is larger than " + b)
+// }
+
+// else if (a < b) {
+//     console.log(b + " is larger than  " + a)
+// }
+
+// else {
+//     console.log("Numbers are equal !")
+// }
+
+
+// Task 3
+
+// if (a > b && a > c) {
+
+//     if (b > c) {
+//         console.log(c, b, a);
+//     }
+//     else if (c > b) {
+//         console.log(b, c, a);
+//     }
+// }
+
+// else if (b > a && b > c) {
+//     if (a > c) {
+//         console.log(c, a, b)
+//     }
+//     else if (c > a) {
+//         console.log(a, c, b)
+//     }
+// }
+
+// else if (c > a && c > b) {
+//     if (a > b) {
+//         console.log(b, a, c)
+//     }
+//     else if (b > a) {
+//         console.log(a, b, c);
+//     }
+// }
+
+
+// Task 4
+
+// let curentYear = 2024;
+
+// if (a < 13 && a > 0 && curentYear == 2024) {
+
+//     switch (a) {
+//         case 1:
+//             console.log("31 days")
+//             break;
+//         case 2:
+//             console.log("28 days")
+//             break;
+//         case 3:
+//             console.log("31 days")
+//             break;
+//         case 4:
+//             console.log("30 days")
+//             break;
+//         case 5:
+//             console.log
+//         case 6:
+//             console.log("30 days")
+//             break;
+//         case 7:
+//             console.log("31 days")
+//             break;
+//         case 8:
+//             console.log("31 days")
+//             break;
+//         case 9:
+//             console.log("30 days")
+//             break;
+//         case 10:
+//             console.log("31 days")
+//             break;
+//         case 11:
+//             console.log("30 days")
+//             break;
+//         case 12:
+//             console.log("31 days")
+//             break;
+
+//     }
+// }
+
+// else if (a < 13 && a > 0) {
+//     switch (a) {
+//         case 1:
+//             console.log("31 days")
+//             break;
+//         case 2:
+//             console.log("29 days")
+//             break;
+//         case 3:
+//             console.log("31 days")
+//             break;
+//         case 4:
+//             console.log("30 days")
+//             break;
+//         case 5:
+//             console.log
+//         case 6:
+//             console.log("30 days")
+//             break;
+//         case 7:
+//             console.log("31 days")
+//             break;
+//         case 8:
+//             console.log("31 days")
+//             break;
+//         case 9:
+//             console.log("30 days")
+//             break;
+//         case 10:
+//             console.log("31 days")
+//             break;
+//         case 11:
+//             console.log("30 days")
+//             break;
+//         case 12:
+//             console.log("31 days")
+//             break;
+
+//     }
+// }
+
+
+// else {
+//     console.log("Invalid month");
+// }
+
+
+// Task 5
+
+// let myMark = 74;
+
+// if (myMark > 90 && myMark <= 100) {
+//     console.log('You get S');
+// }
+
+// else if (myMark > 80 && myMark <= 90) {
+//     console.log('You get A');
+// }
+// else if (myMark > 70 && myMark <= 80) {
+//     console.log('You get B');
+// }
+// else if (myMark > 60 && myMark <= 70) {
+//     console.log('You get C');
+// }
+// else if (myMark > 50 && myMark <= 60) {
+//     console.log('You get D');
+// }
+// else if (myMark > 40 && myMark <= 50) {
+//     console.log('You get E');
+// }
+// else if (myMark >= 0 && myMark <= 40) {
+//     console.log('Student has failed');
+// }
+// else {
+//     console.log('Invalid marks');
+// }
+
+// Task 6
+
+// let year = 2012;
+
+// if (year % 4 == 0 && year % 100 != 0) {
+//     console.log('Year ' + year + ' is a leap year')
+// }
+
+// else if (year % 400 == 0) {
+//     console.log('Year ' + year + ' is a leap year')
+// }
+
+// else {
+//     console.log('Year ' + year + ' is not a leap year')
+
+// }
+
+// Task 7
+
+// let num1 = 4;
+// let num2 = 6;
+// let op = 'subtrac';
+
+// if (op === "add") {
+//     console.log(num1 + num2);
+// }
+// else if (op == 'subtrac') {
+//     console.log(num1 - num2);
+// }
+
+// else if (op == 'multiply') {
+//     console.log(num1 * num2);
+// }
+
+// else if (op == 'divide') {
+//     console.log(num1 / num2);
+// }
+// else {
+//     console.log("Invalid oparation");
+// }
+
+// Task 8
+
+// let side1 = 3;
+// let side2 = 5;
+// let side3 = 5;
+
+// if (side1 == side2 || side1 == side3 || side2 == side3) {
+//     console.log("Isosceles triangle");
+// }
+
+// else if (side1 ** 2 + side2 ** 2 == side3 ** 2) {
+//     console.log("Scalene triangle.");
+// }
+// else if (side1 == side2 == side3) {
+//     console.log("Equilateral triangle")
+
+// }
