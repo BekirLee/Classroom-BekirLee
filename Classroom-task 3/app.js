c111561773d0993ab5e0f1f57dab50683d43567e
BekirLee <nihadbekirli8@gmail.com>
2023-11-28 11:25:46 +0400

    add new codes for new hw task 3

---
+++ Classroom-task 3/app.js
@@ -0,0 +1,14 @@
+// Task1
+
+// let arr = [1, 2, 3, 4, 4, 5, 6, 6, 7];
+// let arr1 = ['a', 'd', 7];
+
+// function clone() {
+//     let newArr = [];
+//     for (let i = 0; i < arr1.length; i++) {
+//         newArr[i] = arr1[i];
+//     }
+//     console.log(newArr);
+// }
+
+// clone();
