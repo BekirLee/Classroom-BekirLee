d55aab02a029724807d1138bcb8d2f26c0633112
BekirLee <nihadbekirli8@gmail.com>
2023-11-28 11:33:55 +0400

    add new codes for new hw task 3

---
+++ Classroom-task 2/script.js
@@ -0,0 +1,107 @@
+// ! Task 1
+
+// let arr1 = [1, 3, 40, 38, 40, 12];
+// let arr2 = [1, 5, 30, 35, 20, 52];
+
+// let sum1 = 0;
+// let sum2 = 0;
+// for (let number1 of arr1) {
+//     sum1 = sum1 + number1;
+// }
+// console.log(sum1);
+
+// for (let number2 of arr2) {
+//     sum2 = sum2 + number2;
+// }
+// console.log(sum2);
+
+// console.log(`result is ${sum1 + sum2}`);
+
+
+// // ! Task 2
+
+// let n = 14;
+
+// for (let i = 2; i <= 14; i += 2) {
+//     console.log(i);
+// }
+
+
+// // ! Task 3
+
+// let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+
+// for (let item = arr.length - 1; item >= 0; item--) {
+//     console.log(arr[item]);
+// }
+
+
+// ! Task 4
+
+// let arr1 = [1, 3, 40];
+// let arr2 = [1, 5, 30];
+
+// for (let item1 = 0; item1 < arr1.length; item1++) {
+//     for (let item2 = 0; item2 < arr2.length; item2++) {
+//         console.log(arr1[item1] + arr2[item2]);
+//     }
+// }
+
+
+// ! Task 5
+
+// let str2 = "dont know wyyy";
+
+// console.log("str2 length: ", str2.length);
+// for (var iterator of str2) {
+//     if (iterator == 'y') {
+//         console.log("Yes")
+//     }
+// }
+
+// if (iterator !== 'y') {
+//     console.log('No')
+// }
+
+// ! Task 6
+
+// let number = 5;
+// var iterator = 1;
+// for (let f = 1; f <= number; f++) {
+//     iterator = f * iterator;
+// }
+// console.log(iterator)
+
+
+// ! Task 7
+
+// let str3 = "racecar";
+// let str4 = "Java";
+
+// for () {
+
+// }
+
+// ! Task 8
+
+
+
+// ! Task 9
+
+// const thisIsAnArray = ["element1", "element2", "element3", "element4"];
+
+// for (let item of thisIsAnArray) {
+//     console.log(item);
+// }
+
+
+// ! Task  10
+
+
+// for (let number = 10; number >= -20; number--) {
+//     if (number % 2 == 0) {
+//         console.log(number);
+//     }
+// }
+
+// Trying it out for practise
--- Classroom-task 3/app.js
+++ Classroom-task 3/app.js
@@ -1,6 +1,7 @@
 // Task1
 
-// let arr = [1, 2, 3, 4, 4, 5, 6, 6, 7];
+let arr = [2, 3, 4, 4, 5, 6, 6, 7];
+let arrABC = ['n', 'i', 'h', 'a', 'd'];
 // let arr1 = ['a', 'd', 7];
 
 // function clone() {
@@ -12,3 +13,33 @@
 // }
 
 // clone();
+
+// Task2
+
+// function firstElement() {
+
+//     if (arr.length !== 0) {
+//         console.log(arr[0]);
+//     }
+
+//     else {
+//         prompt('arrays is empty!')
+//     }
+
+// }
+// firstElement()
+
+// Task 3
+
+// function join() {
+//     let str = '';
+//     for (let i = 0; i < arrABC.length; i++) {
+//         str = str + arrABC[i];
+//     }
+//     console.log(str);
+// }
+
+// join();
+
+// Task 4
+
